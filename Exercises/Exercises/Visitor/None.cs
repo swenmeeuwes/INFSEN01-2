00ad36053c092d3d1d2a70176ee9630ab90637d1
Swen <swenmeeu@gmail.com>
2017-05-02 11:16:16 +0200

    Begin of the 'visitor' design pattern

---
+++ Exercises/Exercises/Visitor/None.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class None<T> : IOption<T>
+    {
+        public U Visit<U>(IOptionVisitor<T, U> visitor)
+        {
+            return visitor.OnNone<U>();
+        }
+    }
+}
---
+++ Exercises/Exercises/Visitor/Some.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    class Some<T> : IOption<T>
+    {
+        T value;
+        public Some(T value)
+        {
+            this.value = value;
+        }
+
+        public U Visit<U>(IOptionVisitor<T, U> visitor)
+        {
+            return visitor.OnSome<U>(this.value);
+        }
+    }
+}
