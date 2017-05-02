1a8ef320bb10d37766cffec7212d4c9d340a312c
swenmeeuwes <swenmeeu@gmail.com>
2017-05-02 19:13:34 +0200

    Added using for SpriteBatch

--- GUILibrary/GUILibrary/GUILibrary/UI/View/View.cs
+++ GUILibrary/GUILibrary/GUILibrary/UI/View/View.cs
@@ -2,6 +2,7 @@
 using GUILibrary.UI.View.State;
 using GUILibrary.Util.Observable;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
