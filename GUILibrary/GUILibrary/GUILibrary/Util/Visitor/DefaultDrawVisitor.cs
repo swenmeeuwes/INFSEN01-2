150872a337a7b6acf51b0844e89874ce481475e8
Swen <swenmeeu@gmail.com>
2017-05-09 15:02:59 +0200

    Beginning of visitor implementation

---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Visitor/DefaultDrawVisitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GUILibrary.UI.Button;
+
+namespace GUILibrary.Util.Visitor
+{
+    class DefaultDrawVisitor : IDrawVisitor
+    {
+        public DefaultDrawVisitor()
+        {
+
+        }      
+        public void Draw(Button element)
+        {
+            
+        }
+    }
+}
---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Visitor/IDrawVisitor.cs
@@ -0,0 +1,14 @@
+﻿using GUILibrary.UI.Button;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Util.Visitor
+{
+    interface IDrawVisitor
+    {
+        void Draw(Button element);
+    }
+}
