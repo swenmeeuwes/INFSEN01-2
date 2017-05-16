f32230833bb0c6692ca62371eb9acc1f3707c65c
swenmeeuwes <swenmeeu@gmail.com>
2017-05-16 17:01:56 +0200

    Implemented CustomListExtension methods

---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Collection/List/CustomListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Util.Collection.List
+{
+    public static class CustomListExtensions
+    {
+        public static CustomList<T> ToCustomList<T>(this IEnumerable<T> enumerable)
+        {
+            var customList = new CustomList<T>();
+            var enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+                customList.Add(enumerator.Current);
+            return customList;
+        }
+    }
+}
