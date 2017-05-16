3e9675d62710f8b62415d53a5de7ddfdf379e6df
Swen <swenmeeu@gmail.com>
2017-05-16 12:56:19 +0200

    Implemented CustomList

---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Collection/List/CustomList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Util.Collection.List
+{
+    class CustomList<T> : IAggregate<T>
+    {
+        private List<T> content;
+        // Allow for index lookup, ex: customList[0]
+        public T this[int key]
+        {
+            get
+            {
+                return content[key];
+            }
+            set
+            {
+                content[key] = value;
+            }
+        }
+
+        public CustomList()
+        {
+            this.content = new List<T>();
+        }
+        public IIterator<T> GetIterator()
+        {
+            return new CustomListIterator<T>(content);
+        }
+
+        // Expose list methods
+        public void Add(T item)
+        {
+            content.Add(item);
+        }
+    }
+}
---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Collection/List/CustomListIterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Util.Collection.List
+{
+    class CustomListIterator<T> : AbstractNumericIndexIterator<T>
+    {
+        public List<T> Content { get; private set; }
+        public CustomListIterator(List<T> content)
+        {
+            Content = content;
+            indexCounter = -1;
+        }
+        public override T Current()
+        {
+            if (indexCounter < 0)
+                throw new Exception("[CustomListIterator] Next() should be called before accessing the current value.");
+
+            return Content[indexCounter];
+        }
+
+        public override bool HasNext()
+        {
+            return indexCounter < Content.Count;
+        }
+
+        public override T Next()
+        {
+            return Content[indexCounter++];
+        }
+    }
+}
