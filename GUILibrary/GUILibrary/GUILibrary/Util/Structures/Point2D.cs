319e51aee70e874888d2417871e4c80984ee5eda
Swen Meeuwes <swenmeeu@gmail.com>
2017-05-29 23:46:41 +0200

    Added Point2D

---
+++ GUILibrary/GUILibrary/GUILibrary/Util/Structures/Point2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Util.Structures
+{
+    /// <summary>
+    /// Represents a point in a 2-dimensional space
+    /// </summary>
+    class Point2D<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public Point2D(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }        
+    }
+}
