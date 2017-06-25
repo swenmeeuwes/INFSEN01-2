d744c66eadea85bbc66bc7ccf4a0f799625e7c4f
Swen Meeuwes <swenmeeu@gmail.com>
2017-06-25 16:14:08 +0200

    Implemented application adapter

---
+++ GUILibrary/GUILibrary/GUILibrary/Application/Controller/IApplicationAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Application.Controller
+{
+    interface IApplicationAdapter
+    {
+        void Exit();
+    }
+}
---
+++ GUILibrary/GUILibrary/GUILibrary/Application/Controller/MonoGameApplicationAdapter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUILibrary.Application.Controller
+{
+    class MonoGameApplicationAdapter : IApplicationAdapter
+    {
+        private Game application;
+        public MonoGameApplicationAdapter(Game application)
+        {
+            this.application = application;
+        }
+        public void Exit()
+        {
+            application.Exit();
+        }
+    }
+}
